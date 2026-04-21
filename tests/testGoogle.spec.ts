09f62370b2261c607e91551ff46a2673e192827c
Pri Aragao <priscillaaragao@msn.com>
2026-04-21 10:46:50 -0400

    ajustes testes

--- tests/testGoogle.spec.ts
+++ tests/testGoogle.spec.ts
@@ -1,20 +1,36 @@
 import { test, expect } from "@playwright/test";
-import { pegarValorJson } from "../resources/jsonHelper.js";
+// import { pegarValorJson } from "../resources/jsonHelper.js";
 import { TestPage } from "../pages/testPage.js";
+import massa from "../testdata/massa.json" with { type: "json" };
 
 test("Caso de teste usando massa em JSON", async ({ page }) => {
-  const SEQUENCIAL = 0;
-
-  const url = pegarValorJson(SEQUENCIAL, "URL");
-  const pesquisa = pegarValorJson(SEQUENCIAL, "pesquisa");
+  const dados = massa[0];
 
   const testPage = new TestPage(page);
 
-  await testPage.acessarUrl(url);
-  await testPage.pesquisar(pesquisa);
+  if (!dados) {
+    throw new Error("Nenhuma massa encontrada no arquivo JSON");
+  }
+
+  await testPage.acessarUrl(dados.URL);
+  await testPage.pesquisar(dados.pesquisa);
 
   await expect(page).toHaveURL(/google\.com/);
 });
+
+// test("Caso de teste usando massa em JSON", async ({ page }) => {
+//   const SEQUENCIAL = 0;
+
+//   const url = pegarValorJson(SEQUENCIAL, "URL");
+//   const pesquisa = pegarValorJson(SEQUENCIAL, "pesquisa");
+
+//   const testPage = new TestPage(page);
+
+//   await testPage.acessarUrl(url);
+//   await testPage.pesquisar(pesquisa);
+
+//   await expect(page).toHaveURL(/google\.com/);
+// });
 
 // import massa from '../testdata/massa.json';
 
